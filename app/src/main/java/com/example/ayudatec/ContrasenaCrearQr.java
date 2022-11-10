ebb9bb52211a86169db22718e57ee7fb49c4bb2b
TheSkrapy <gregocastillo07@hotmail.com>
2022-11-10 10:56:40 -0600

    Creacion de plantillas

---
+++ app/src/main/java/com/example/ayudatec/ContrasenaCrearQr.java
@@ -0,0 +1,14 @@
+package com.example.ayudatec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContrasenaCrearQr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contrasena_crear_qr);
+    }
+}
--- app/src/main/java/com/example/ayudatec/MainActivity.java
+++ app/src/main/java/com/example/ayudatec/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.ayudatec;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnEscanear;
+    Button btnGenerar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnEscanear = (Button) findViewById(R.id.btnScan);
+        btnGenerar=(Button) findViewById(R.id.btnCrear);
+
+
+        btnEscanear.setOnClickListener(view -> {
+            Intent c = new Intent(MainActivity.this, Escaner.class);
+            startActivity(c);
+        });
+
+        btnGenerar.setOnClickListener(view -> {
+            Intent c = new Intent(MainActivity.this, ContrasenaCrearQr.class);
+            startActivity(c);
+        });
+
     }
 }
